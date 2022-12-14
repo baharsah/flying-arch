2088b144d7a821fa07898747fa02710aea4a2f10
baharsah <me@baharsah.my.id>
2022-12-14 09:17:34 +0700

    update

--- src/components/DetailTour.jsx
+++ src/components/DetailTour.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import { Container } from 'react-bootstrap'
+import ImageExpo from './ImageExpo'
 
 function DetailTour() {
   return (
     <>
-    <Container style={{height : "50px"}}></Container>
-    <Container className='mt-5'>
-        <h1 className='mt-1 text-center'> Detail Trip</h1>
-        <p className='text-center'>Detail trip you want to sleep at.</p>
+    {/* <Container style={{height : "50px"}}></Container> */}
+    <Container className='mt-5 pt-5 ms-5 ps-5'>
+      {/* Whitespaces */}
     </Container>
+    <ImageExpo>
+
+    </ImageExpo>
     </>
   )
 }
